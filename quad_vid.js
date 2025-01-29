f88650eddea00f9b3d26ed621491ef13f6acb19a
Niko Severino <severinoniko@gmail.com>
2025-01-28 20:32:07 -0500

    quad cam thing

--- quad_vid.js
+++ quad_vid.js
@@ -1,17 +1,20 @@
+bmp = 3000
 // Init webcamera
 s0.initCam(2)
 s1.initCam(1)
 
+color_array = [0,1,2,3,4,5,6]
+
 // top left
 src(s0)
-  .color(1, 1, 1)
+  .color(color_array, 1, 1)
   .modulate(shape(24))
   .out(o0)
 
 //top right
 src(s0)
 // color set
-  .color(1, 0, 10)
+  .color(1, color_array, 10)
 // wavey
   .modulate(voronoi())
   .pixelate(1000000000000)
@@ -24,7 +27,7 @@ src(s0)
 
 // bottom right
 src(s0)
-  .color(1, 0, 1)
+  .color(1, color_array, 1)
   .modulateRotate(osc(10), 2)
   .out(o3)
 

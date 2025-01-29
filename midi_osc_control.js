86d70b9f2116d4558515ce990656b34502aea8a4
Niko Severino <severinoniko@gmail.com>
2025-01-28 20:30:56 -0500

    add more knobs

--- midi_osc_control.js
+++ midi_osc_control.js
@@ -1,6 +1,12 @@
 s0.initCam(2)
-osc_val = () => (cc[1] * 100)
+
+// modulate camera stream with four knobs
+osc_val = () => (cc[4] * 100)
+r = () => (cc[1] * 2.5) 
+g = () => (cc[2] * 2.5)
+b = () => (cc[3] * 2.5)
+
 src(s0)
-  .color(1, 0, 3)
+  .color(r, g, b)
   .modulate(osc(osc_val))
   .out() 
